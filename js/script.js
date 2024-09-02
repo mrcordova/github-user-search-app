38b19e8d55ab979d5a4796ba7fb6238e838613a5
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-02 16:37:31 -0700

    logic for adding dynamic profile content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const GITHUB_API = "https://api.github.com/users/";
 let userName = "octocat";
 const profileImg = document.querySelector(".profile-pic>img");
+let nameEle = document.querySelector(".name-container>h2");
+let username = document.querySelector(".name-container>h3");
+let date = document.querySelector(".date-container>span");
+let bioPara = document.querySelector(".bio");
+let stats = document.querySelectorAll(
+  "p[data-repo], p[data-followers], p[data-following]"
+);
+let links = document.querySelectorAll(".links>div");
 
 window.addEventListener("load", async () => {
   const githubResponse = await fetch(`${GITHUB_API}${userName}`);
@@ -11,7 +19,7 @@ window.addEventListener("load", async () => {
     created_at,
     followers,
     following,
-    public_repo,
+    public_repos,
     blog,
     name,
     login,
@@ -19,5 +27,34 @@ window.addEventListener("load", async () => {
     location,
     twitter_username,
   } = await githubResponse.json();
-  //   profileImg.setAttribute("src", avatar_url);
+
+  //   console.log(await githubResponse.json());
+  profileImg.setAttribute("src", avatar_url);
+  nameEle.textContent = name;
+  username.textContent = `@${login}`;
+  date.textContent = created_at;
+  bioPara.textContent = bio != null ? bio : "This profile has no bio";
+  bioPara.classList.toggle("unavailable", bio == null);
+
+  stats[0].textContent = public_repos;
+  stats[0].dataset.repo = public_repos;
+  stats[1].textContent = followers;
+  stats[1].dataset.followers = followers;
+  stats[2].textContent = following;
+  stats[2].dataset.following = following;
+
+  links[0].children[1].textContent =
+    location != null ? location : "Not available";
+  links[0].classList.toggle("unavailable", location == null);
+
+  links[1].children[1].textContent = blog != null ? blog : "Not available";
+  links[1].classList.toggle("unavailable", blog == null);
+
+  links[2].children[1].textContent =
+    twitter_username != null ? twitter_username : "Not available";
+  links[2].classList.toggle("unavailable", twitter_username == null);
+
+  links[3].children[1].textContent =
+    company != null ? company : "Not available";
+  links[3].classList.toggle("unavailable", company == null);
 });
