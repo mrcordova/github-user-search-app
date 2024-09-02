6f4661f923ec0f2171c1ed46b7836be0c5028f34
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-02 16:46:55 -0700

    styling and logic for links

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ window.addEventListener("load", async () => {
 
   //   console.log(await githubResponse.json());
   profileImg.setAttribute("src", avatar_url);
-  nameEle.textContent = name;
+  nameEle.textContent = name != null ? name : login;
   username.textContent = `@${login}`;
   date.textContent = created_at;
   bioPara.textContent = bio != null ? bio : "This profile has no bio";
@@ -44,17 +44,27 @@ window.addEventListener("load", async () => {
   stats[2].dataset.following = following;
 
   links[0].children[1].textContent =
-    location != null ? location : "Not available";
+    location != null ? location : "Not Available";
+
   links[0].classList.toggle("unavailable", location == null);
 
-  links[1].children[1].textContent = blog != null ? blog : "Not available";
+  links[1].children[1].textContent = blog != null ? blog : "Not Available";
+  links[1].children[1].setAttribute("href", `${blog}`);
   links[1].classList.toggle("unavailable", blog == null);
 
   links[2].children[1].textContent =
-    twitter_username != null ? twitter_username : "Not available";
+    twitter_username != null ? twitter_username : "Not Available";
+  links[2].children[1].setAttribute(
+    "href",
+    `htttps://x.com/${twitter_username}`
+  );
   links[2].classList.toggle("unavailable", twitter_username == null);
 
   links[3].children[1].textContent =
-    company != null ? company : "Not available";
+    company != null ? company : "Not Available";
+  links[3].children[1].setAttribute(
+    "href",
+    `https://github.com/${company.slice(1)}`
+  );
   links[3].classList.toggle("unavailable", company == null);
 });
