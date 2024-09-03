d81bb5e22f5402769545b678fecc29dbefe0570b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-03 09:59:13 -0700

    logic to show no results

--- js/script.js
+++ js/script.js
@@ -29,62 +29,69 @@ const months = {
 
 async function updateProfile() {
   const githubResponse = await fetch(`${GITHUB_API}${userName}`);
-  const {
-    avatar_url,
-    bio,
-    created_at,
-    followers,
-    following,
-    public_repos,
-    blog,
-    name,
-    login,
-    company,
-    location,
-    twitter_username,
-  } = await githubResponse.json();
 
-  const dateText = new Date(created_at);
-  profileImg.setAttribute("src", avatar_url);
-  nameEle.textContent = name != null ? name : login;
-  username.textContent = `@${login}`;
-  date.textContent = `${dateText.getDate()} ${
-    months[dateText.getMonth()]
-  } ${dateText.getFullYear()}`;
-  bioPara.textContent = bio != null ? bio : "This profile has no bio";
-  bioPara.classList.toggle("unavailable", bio == null);
+  if (githubResponse.status == 200) {
+    const {
+      avatar_url,
+      bio,
+      created_at,
+      followers,
+      following,
+      public_repos,
+      blog,
+      name,
+      login,
+      company,
+      location,
+      twitter_username,
+    } = await githubResponse.json();
 
-  stats[0].textContent = public_repos;
-  stats[0].dataset.repo = public_repos;
-  stats[1].textContent = followers;
-  stats[1].dataset.followers = followers;
-  stats[2].textContent = following;
-  stats[2].dataset.following = following;
+    const dateText = new Date(created_at);
+    profileImg.setAttribute("src", avatar_url);
+    nameEle.textContent = name != null ? name : login;
+    username.textContent = `@${login}`;
+    date.textContent = `${dateText.getDate()} ${
+      months[dateText.getMonth()]
+    } ${dateText.getFullYear()}`;
+    bioPara.textContent = bio != null ? bio : "This profile has no bio";
+    bioPara.classList.toggle("unavailable", bio == null);
 
-  links[0].children[1].textContent =
-    location != null ? location : "Not Available";
+    stats[0].textContent = public_repos;
+    stats[0].dataset.repo = public_repos;
+    stats[1].textContent = followers;
+    stats[1].dataset.followers = followers;
+    stats[2].textContent = following;
+    stats[2].dataset.following = following;
 
-  links[0].classList.toggle("unavailable", location == null);
+    links[0].children[1].textContent =
+      location != null ? location : "Not Available";
 
-  links[1].children[1].textContent = blog != null ? blog : "Not Available";
-  links[1].children[1].setAttribute("href", `${blog}`);
-  links[1].classList.toggle("unavailable", blog == null);
+    links[0].classList.toggle("unavailable", location == null);
 
-  links[2].children[1].textContent =
-    twitter_username != null ? twitter_username : "Not Available";
-  links[2].children[1].setAttribute(
-    "href",
-    `htttps://x.com/${twitter_username}`
-  );
-  links[2].classList.toggle("unavailable", twitter_username == null);
+    links[1].children[1].textContent = blog != null ? blog : "Not Available";
+    links[1].children[1].setAttribute("href", `${blog}`);
+    links[1].classList.toggle("unavailable", blog == null);
 
-  links[3].children[1].textContent =
-    company != null ? company : "Not Available";
-  links[3].children[1].setAttribute(
-    "href",
-    `https://github.com/${company?.slice(1)}`
-  );
-  links[3].classList.toggle("unavailable", company == null);
+    links[2].children[1].textContent =
+      twitter_username != null ? twitter_username : "Not Available";
+    links[2].children[1].setAttribute(
+      "href",
+      `http://twitter.com/${twitter_username}`
+    );
+    links[2].classList.toggle("unavailable", twitter_username == null);
+
+    links[3].children[1].textContent =
+      company != null ? company : "Not Available";
+    links[3].children[1].setAttribute(
+      "href",
+      `https://github.com/${company?.slice(1)}`
+    );
+    links[3].classList.toggle("unavailable", company == null);
+    searchInput.nextElementSibling.classList.toggle("hide", true);
+  } else {
+    console.error("no results");
+    searchInput.nextElementSibling.classList.toggle("hide", false);
+  }
 }
 
 window.addEventListener("load", async () => {
@@ -97,6 +104,7 @@ toggleBtn.addEventListener("click", () => {
 
 searchBtn.addEventListener("click", async (e) => {
   // console.log(searchInput.value);
+
   userName = searchInput.value;
   await updateProfile();
 });
